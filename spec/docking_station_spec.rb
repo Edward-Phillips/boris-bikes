ab2e3cb010267c03769d572a21e748fa8b9e5a57
Faye Carter <fayecarter333@gmail.com>
2020-03-17 15:04:44 +0000

    Add DockingStations Class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,5 @@
-require "dockingstation"
+require 'docking_station'
 
 describe  DockingStation do
-    subject(:docking_station) { described_class.new }
 
-    it 'initializes new docking station' do
-      expect(docking_station).to_not eq nil
-    end
 end
