f98b9178363043b58070d4d3fd48c398fe17bab1
Edward Phillips <edd0ddi0@gmail.com>
2020-03-17 17:27:00 +0000

    challenge 11 in progress

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,16 @@
-require 'bike'
+require './lib/bike'
 
 class DockingStation
+
   def release_bike
     Bike.new
   end
+
+  def docker(bike)
+    @dock.push(bike)
+  end
+
+def contents
+end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,12 +2,21 @@ require 'docking_station'
 
 describe DockingStation do
   subject(:NewDockingStation) { described_class.new}
-    it { should respond_to :release_bike } 
+    it { should respond_to :release_bike }
 
     it 'bike should respond to working?' do
       bike = subject.release_bike
       expect(bike).to be_working
     end
-  
-end
 
+=begin
+    it 'should be able to dock bikes.' do
+      bike = subject.release_bike
+      expect(subject.dock(bike)).to be
+    end
+=end
+    it { is_expected.to respond_to(:docker).with(1).argument}
+    it { is_expected.to respond_to(:contents)}
+
+
+end
