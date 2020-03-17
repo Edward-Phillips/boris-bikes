773a790e39569d0803a004dafc775c5222ec1505
Faye Carter <fayecarter333@gmail.com>
2020-03-17 16:28:29 +0000

    Bike connected to docking station class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,5 +3,11 @@ require 'docking_station'
 describe DockingStation do
   subject(:NewDockingStation) { described_class.new}
     it { should respond_to :release_bike } 
+
+    it 'bike should respond to working?' do
+      bike = subject.release_bike
+      expect(bike).to be_working
+    end
+  
 end
 
