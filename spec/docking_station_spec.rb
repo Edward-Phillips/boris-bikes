600199cf94eb18bce814d25c7a6ca82b12161a88
Faye Carter <fayecarter333@gmail.com>
2020-03-17 15:46:05 +0000

    Docking Station responds to release_bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,7 @@
 require 'docking_station'
 
-describe  DockingStation do
-
+describe DockingStation do
+  subject(:NewDockingStation) { described_class.new}
+    it { should respond_to :release_bike } 
 end
+
